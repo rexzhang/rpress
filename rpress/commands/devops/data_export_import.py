83f8d9903aca54cdfef0149775b75434b44bb9d7
Rex Zhang <rex.zhang@gmail.com>
2018-06-13 16:44:41 +0800

    Add export all site data at CLI

--- rpress/commands/devops/data_export_import.py
+++ rpress/commands/devops/data_export_import.py
@@ -5,12 +5,14 @@
 import click
 from flask.cli import with_appcontext
 
+from rpress.models import Site
 from rpress.runtimes.data_export_import_v1 import export_site_data_to_json
 
 
 @click.command()
 @with_appcontext
 def command_export():
-    site_id = 1
-    export_site_data_to_json(int(site_id))
+    for site in Site.query:
+        export_site_data_to_json(site.id)
+
     return
