2ee84327b90fbe4e9b1996a18ff41bf120f6b236
Rex Zhang <rex.zhang@gmail.com>
2018-06-20 23:03:42 +0800

    Fix missed "host.docker.internal" at linux@docker https://dev.to/bufferings/access-host-from-a-docker-container-4099

---
+++ script/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# https://dev.to/bufferings/access-host-from-a-docker-container-4099
+# https://github.com/bufferings/docker-access-host
+
+HOST_DOMAIN="host.docker.internal"
+ping -q -c1 "$HOST_DOMAIN" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+fi
+
+# Here is the original entry point.
+curl -sS host.docker.internal:8888
+cat /etc/hosts
