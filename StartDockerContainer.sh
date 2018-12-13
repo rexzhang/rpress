ea2228d2f3f47052b48b75bab431dc3c3b331380
Rex Zhang <rex.zhang@gmail.com>
2018-12-13 20:45:45 +0800

    Add sgfans.org

--- StartDockerContainer.sh
+++ StartDockerContainer.sh
@@ -3,4 +3,4 @@
 docker build -t ray1ex/rpress .
 docker rmi -f $(docker images -qa -f "dangling=true")
 
-docker run -dit -p 127.0.0.1:10000:5000 --restart unless-stopped ray1ex/rpress
+docker run -dit -p 127.0.0.1:5000:5000 --restart unless-stopped ray1ex/rpress
